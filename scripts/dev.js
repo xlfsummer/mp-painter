2d79815bc00c508e26145f02a2bff9dd789e85ba
xlfsummer <xlf-summer@outlook.com>
2019-09-02 15:02:21 +0800

    编写 dev 脚本, 更新包版本

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,30 +1,34 @@
 
 let cp = require("child_process");
-let { promisify } = require("util");
 let fs = require("fs");
+let os = require("os");
+let path = require("path");
 
-process.stdout.write(
-    cp.spawnSync(
-        "node", ["C:\\Program Files\\nodejs\\node_modules\\npm", "run", "build"]
-).stdout);
+let npm = os.platform() == "win32" ? "npm.cmd" : "npm";
 
-process.stdout.write(
-    cp.spawnSync(
-        "node", ["C:\\Program Files\\nodejs\\node_modules\\npm", "pack"]
-    ).stdout
-);
-
-let fileNameBuffer = cp.spawnSync(
-    "node", ["C:\\Program Files\\nodejs\\node_modules\\npm", "pack"]
-).stdout;
-
-let fileName = Buffer.from(fileNameBuffer).toString();
-
-// console.log(data);
-
-// todo
 // npm run build
-// npm pack
-// mv [package_name_version] example
-// cd example
-// npm install [package_name_version]
+console.time("编译 ts");
+let buildResult = cp.spawnSync(npm, ["run", "build"])
+// process.stdout.write(buildResult.stdout);
+// process.stdout.write(buildResult.stderr);
+console.timeEnd("编译 ts");
+
+console.time("打包文件");
+let packResult = cp.spawnSync(npm, ["pack"]);
+let fileNameBuffer = packResult.stdout;
+let fileName = Buffer.from(fileNameBuffer).toString().trim();
+console.timeEnd("打包文件");
+
+console.time("移动包文件");
+fs.renameSync(fileName, "example" + path.delimiter + fileName);
+console.timeEnd("移动包文件");
+
+console.time("安装包");
+let installResult = cp.spawnSync(npm, ["install", fileName], { cwd: "example" });
+// process.stdout.write(installResult.stdout);
+// process.stdout.write(installResult.stderr);
+console.timeEnd("安装包");
+
+console.time("删除已安装的包文件");
+fs.unlinkSync("example" + path.delimiter + fileName);
+console.timeEnd("删除已安装的包文件");
