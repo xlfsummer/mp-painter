3b4b09fe11be3fe8f371a256c453a62767ec9886
xlfsummer <xlf-summer@outlook.com>
2019-11-13 15:16:58 +0800

    修复图片绘制

--- src/lib/paint-image.ts
+++ src/lib/paint-image.ts
@@ -29,19 +29,12 @@ export default async function paintImage(this: Painter, image: CanvasImage){
     // 图片内容的尺寸
     let contentSize: Rect;
     if(objectFit == "contain"){
-      contentSize = await calculateContainSize(image)
+      contentSize = await calculateContainSize(image);
     } else { // fill
       contentSize = { left, top, width, height }
     }
-
-    let src: string;
-    // 支付宝中需要先下载图片再绘制
-    if(this.platform == "mp-alipay" && !/^https:\/\/resource\//.test(image.src)){
-      src = await downloadFileToLocal(image.src).catch(err => console.log("下载错误: ", err)) || "";
-    }else{
-      src = image.src;
-    }
-    
+    let src = await getDrawableImageSrc(this, image);
+    console.log("调用小程序绘制，使用:", src);
     if(src){
       this.ctx.drawImage(
         src,
@@ -56,6 +49,25 @@ export default async function paintImage(this: Painter, image: CanvasImage){
       width,
       height
     };
+}
+
+async function getDrawableImageSrc(painter: Painter, image: CanvasImage) {
+  let platform = painter.platform;
+  /** @expample "https://resource/1573628995676.jpg" */
+  const ALIPAY_LOCAL_RESOURCE_URL_REG = /^https:\/\/resource\/\d+\.\w+$/;
+  const WEIXIN_LOCAL_RESOURCE_URL_REG = /^wxfile:/;
+  let shouldDownload = 
+    // 支付宝中需要先下载图片再绘制
+    platform == "mp-alipay" && !ALIPAY_LOCAL_RESOURCE_URL_REG.test(image.src) ||
+    // 微信小程序开发者工具中不需要先下载再绘制, 但在手机中预览时需要
+    platform == "mp-weixin" && !WEIXIN_LOCAL_RESOURCE_URL_REG.test(image.src)
+    // 百度小程序开发者工具/手机中不需要下载文件即可绘制
+  ;
+
+  if (!shouldDownload) return image.src;
+
+  console.log("绘制图片: 下载图片文件:", image.src);
+  return await downloadFileToLocal(image.src).catch(err => console.log("下载错误: ", err)) || "";
 }
 
 async function calculateContainSize(image: CanvasImage): Promise<Rect>{
