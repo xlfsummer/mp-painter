c9b37daf727c08a5948617adee6de3cd5af7dc45
xlfsummer <xlf-summer@outlook.com>
2020-05-13 21:49:37 +0800

    增加微信小程序示例，支持微信小程序中引用

--- example_wx/pages/index/index.js
+++ example_wx/pages/index/index.js
@@ -8,10 +8,32 @@ Page({
     let ctx = wx.createCanvasContext('canvas');
     let painter = new Painter(ctx);
     painter.draw({
-      type: "rect",
-      width: 700,
-      height: 40,
-      background: "#f44"
+      type: "container",
+      children: [
+        {
+          top: 20,
+          left: 350,
+          type: "text",
+          align: "center",
+          position: "absolute",
+          fontSize: 100,
+          content: "mp-painter",
+          fontWeight: "bold",
+          fontStyle: "italic",
+          color: "#f44"
+        },
+        {
+          top: 30,
+          left: 360,
+          type: "text",
+          position: "absolute",
+          align: "center",
+          fontSize: 100,
+          content: "mp-painter",
+          fontWeight: "bold",
+          fontStyle: "italic"
+        }
+      ]
     })
   }
 })
