08ea0ddfb73030b5c7a9bd75328deaa4d4d52bed
xlfsummer <xlf-summer@outlook.com>
2020-11-25 00:21:37 +0800

    feat: 补充 h5 示例

--- example_web/src/App.vue
+++ example_web/src/App.vue
@@ -18,7 +18,7 @@ export default {
       /** @type {HTMLCanvasElement} */
       let canvasEl = canvas.value;
       canvasEl.width = 800;
-      canvasEl.height = 800;
+      canvasEl.height = 1000;
 
       let ctx = canvasEl.getContext("2d");
 
@@ -65,7 +65,23 @@ export default {
             { type: "rect", left: 10, borderRadius: 10, width: 50, height: 50, background: "#ee6" },
             { type: "rect", left: 10, borderRadius: [10, 30, 10, 30], width: 50, height: 50, background: "#66e" },
             { type: "rect", left: 10, borderRadius: 50, width: 50, height: 50, background: "#6e6" }
-          ]}
+          ]},
+          { type: "text", top: 30, content: "文本块 text-block" },
+          { type: "text-block", top: 10, fontSize: 22, lineHeight: 30, lineClamp: 3, width: 500, content: "示例文本".repeat(30)},
+          { type: "text", top: 30, content: "裁剪 clip" },
+          { type: "clip",
+            path: { type: "rounded-rect", top: 10, left: 10, width: 100, height: 100, borderRadius: 50 },
+            content: { type: "text-block", fontSize: 22, lineHeight: 30, lineClamp: 4, width: 150, content: "示例文本".repeat(30)},
+          },
+          { type: "text", top: 30, content: "变换 transform" },
+          {
+            type: "transform",
+            transform: [
+              { type: "translate", x: 20, y: 20 },
+              { type: "rotate", rotate: 45 },
+            ],
+            content: { type: "rect", width: 50, height: 50, background: "#6ee" },
+          }
         ]
       });
     });
@@ -77,3 +93,8 @@ export default {
 }
 </script>
 
+<style scoped>
+canvas {
+  border: 1px #ddd solid;
+}
+</style>
