78250153bc70a7ca3b2bd0be277ddd0c91f09238
xlfsummer <xlf-summer@outlook.com>
2020-11-30 23:59:44 +0800

    refactor: 优化代码

--- example/src/pages/playground/playground.vue
+++ example/src/pages/playground/playground.vue
@@ -1,10 +1,15 @@
 <template>
     <view class="page">
         <view class="page-title h2">来试试！绘制后复制 url 来分享！</view>
+        
         <textarea v-model="elementOptionText" class="textarea" maxlength="-1"/>
         <button @click="update">绘制</button>
+
+        <view style="color: red">{{ errorMessage }}</view>
+
 		<canvas canvas-id="canvas-playground" id="canvas-playground" class="canvas" 
         :style="{ width: canvasSize.width + 'rpx', height: canvasSize.height + 'rpx' }"/>
+
 		<web-link href="https://github.com/xlfsummer/mp-painter/tree/master/example/src/pages/playground/playground.vue"/>
     </view>
 </template>
@@ -20,26 +25,25 @@ const defaultElementOption = /** @type {ElementOption} */(
         { type: "text", fontFamily: "times", content: "Hello, World!", fontStyle: "italic", color: "#456" },
         { type: "rect", top: 5, background: "#456", width: 80, height: 3 },
         { type: "rect", height: 20 },
-    ] }
+    ]}
 );
 
 export default {
-    onLoad(option){
-        console.log(this.$route);
-        if(option.d){
-            try {
-                this.elementOption = JSON.parse(decodeURIComponent(option.d));
-            } catch {
-                this.elementOptionText = decodeURIComponent(option.d);
-            }
-        } else {
-            this.elementOption = defaultElementOption;
+    onLoad({ d = encodeURIComponent(JSON.stringify(defaultElementOption)) } = {}){
+
+        const elementOptionText = decodeURIComponent(d);
+        try {
+            this.elementOption = JSON.parse(elementOptionText);
+        } catch {
+            this.elementOptionText = elementOptionText;
         }
-        this.update();
+
+        this.draw();
     },
     data(){
         return {
             canvasSize: { width: 600, height: 0 },
+            errorMessage: "",
             elementOptionText: ""
         };
     },
@@ -55,24 +59,23 @@ export default {
             set(v){
                 this.elementOptionText = JSON.stringify(v, null, 2);
             }
-        },
-        elementOptionTextIsValid(){
-            try { return JSON.parse(this.elementOptionText), true; }
-            catch { return false }
         }
     },
     methods: {
+        validJson(){
+            try {
+                JSON.parse(this.elementOptionText);
+            } catch(e) {
+                this.errorMessage = e.message;
+                throw e;
+            }
+        },
         update(){
+            this.validJson();
             this.updateHash();
-            
-            // 清除上一次绘制的内容
-            new Painter(uni.createCanvasContext("canvas-playground")).draw({
-                type: "rect",
-                background: "#fff",
-                width: this.canvasSize.width,
-                height: this.canvasSize.height
-            });
-
+        },
+        draw(){
+            this.validJson();
             new Painter(uni.createCanvasContext("canvas-playground")).draw(this.elementOption, {
                 afterLayout: async size => {
                     // 获取 painter 布局计算之后得出的高度，并更新 canvas 的高度
@@ -83,18 +86,18 @@ export default {
                 }
             });
         },
-        /** 更新 url hash, 以便分享 */
+        /** 更新 url hash */
         updateHash(){
+            // 移除缩进，节省空间
+            const elementOptionTextWithoutIndent = JSON.stringify(this.elementOption)
 
-            if(window && this.elementOptionTextIsValid){
-
-                // 移除缩进，节省空间
-                const elementOptionTextWithoutIndent = JSON.stringify(this.elementOption)
-
-                window.location.hash = this.$route.path 
-                    + "?"
-                    + new URLSearchParams({d: elementOptionTextWithoutIndent}).toString();
-            }
+            uni.navigateTo({
+                url: this.$route.path 
+                + "?"
+                + new URLSearchParams({
+                    d: elementOptionTextWithoutIndent
+                })
+            })
         }
     }
 }
